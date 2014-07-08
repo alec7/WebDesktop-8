6080efaed62f7a63bca55002fb6428cd4142ceb1
anttribe <anshenglimin@gmail.com>
2014-07-08 19:59:05 +0800

    update weather

--- resources/widgets/weather/Weather.js
+++ resources/widgets/weather/Weather.js
@@ -15,6 +15,7 @@
     var Weather = function () {
         return {
             _options: {}, //参数
+            _weather: null,
             /**
              * 初始化
              * @param elem
@@ -23,6 +24,23 @@
              */
             _initialize: function (elem, options) {
                 this._setOptions(options);
+
+                var weather = $('<div>', {
+                    class: 'ui-weather',
+                    html: [
+                        $('<div>', {
+                            class: 'ui-weather-currTemp'
+                        }),
+                        $('<div>', {
+                            class: 'ui-weather-state'
+                        }),
+                        $('<div>', {
+                            class: 'ui-weather-weatherInfo'
+                        })
+                    ]
+                }).appendTo($(elem));
+
+                this._weather = weather;
             },
             /**
              * 设置参数
@@ -44,6 +62,12 @@
                 if (elem && $(elem).length > 0) {
                     this._initialize(elem, options);
                 }
+            },
+            /**
+             * 展示天气
+             */
+            show: function () {
+                //获取本地ip
             }
         }
     }();
